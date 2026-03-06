1e5738b135c14b079c09b7c7ddfaa8c1045a2f3b
JibhakateApurva <apurvajibhakate007@gmail.com>
2026-03-07 01:25:07 +0530

    latest changes added

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite' // Jar plugin vaprat asal tar
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // He v4 saathi navin Vite plugin aahe
+    tailwindcss(),
   ],
+  base: '/',
 })
+
